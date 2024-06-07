9d2d54e90a19aa170d2f7e8346e21c201da6c605
Emmanuel Denis Taban <emmadenistaban@gmail.com>
2024-06-07 14:54:25 +0300

    Add files via upload

---
+++ Bukoto Village Bank/WhileLoop.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numMembers;
+        double[] principalAmounts;
+
+        System.out.print("Enter the number of members: ");
+        numMembers = scanner.nextInt();
+        principalAmounts = new double[numMembers];
+
+        int i = 0;
+        while (i < numMembers) {
+            System.out.print("Enter principal amount for member " + (i + 1) + ": ");
+            principalAmounts[i] = scanner.nextDouble();
+            i++;
+        }
+        double interestRate = 0.05; 
+        int timeFrame = 12; 
+        System.out.println("Value of investment after " + timeFrame + " months:");
+        i = 0;
+        while (i < numMembers) {
+            double investmentValue = principalAmounts[i] * Math.pow(1 + interestRate, timeFrame);
+            System.out.println("Member " + (i + 1) + ": " + investmentValue);
+            i++;
+        }
+
+        scanner.close();
+    }
+}
